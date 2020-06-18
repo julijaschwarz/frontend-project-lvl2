ef48196ef611bdc918acb8f7fcf4dd5e7c78e102
JulSh <fr.schwarz@yandex.ru>
2020-06-18 16:52:09 +0300

    Step two

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/445c8f9a4c1b154325e0/maintainability)](https://codeclimate.com/github/julijaschwarz/frontend-project-lvl2/maintainability)
 
 ![Lint](https://github.com/julijaschwarz/frontend-project-lvl2/workflows/Lint/badge.svg)
+
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,14 @@
 #!/usr/bin/env node
+
+import program from 'commander';
+
+program
+  .version('0.0.1', '-v, --vers', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .action(function (filepath1, filepath2) {
+
+  })
+  .option('-f, --format [type]', 'output format');
+
+program.parse(process.argv);
